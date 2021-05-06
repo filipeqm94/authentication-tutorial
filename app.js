24a7442a2ad27c51fe0326e40e0c02fd3594a4f4
Filipe <queiroz.filipe94@gmail.com>
2021-05-06 14:16:02 -0400

    :sparkels: UPDATE: Require and utilize md5 package to hash user's password

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption"); //Used for encryption
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,10 +27,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
+// userSchema.plugin(encrypt, {     // Used for encryption
+//   secret: process.env.SECRET,
+//   encryptedFields: ["password"],
+// });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,7 +45,7 @@ app
   })
   .post((req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, (err, foundUser) => {
       if (err) {
@@ -70,7 +71,7 @@ app
 
     const newUser = new User({
       email: username,
-      password,
+      password: md5(password),
     });
 
     newUser.save((err) => {
