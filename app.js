85689ab9e6e7b53a77a40057dffd6d62674fee69
Filipe <queiroz.filipe94@gmail.com>
2021-05-06 16:15:16 -0400

    :sparkles: UPDATE: Implemnet Level 5 Security to application, using Passport to add Cookies and Sessions

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption"); <--- Used for encrypting
 // const md5 = require("md5");    <-------------------- Used for Hashing
-const bcrypt = require("bcrypt");
+
+/* 
+// <---------- Used for Salting and Hasing ---------->
+const bcrypt = require("bcrypt"); 
 const saltRounds = 10;
+*/
+
+// <----------- Level 5 --------->
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -19,15 +28,29 @@ app.use(
   })
 );
 
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
-  email: String,
+  username: String,
   password: String,
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 // userSchema.plugin(encrypt, {     // Used for encryption
 //   secret: process.env.SECRET,
@@ -36,10 +59,78 @@ const userSchema = new mongoose.Schema({
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+// <---------- Level 5 ---------->
+
+app.route("/secrets").get((req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    User.register(
+      { username: req.body.username },
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          console.log(err);
+          res.redirect("/register");
+        } else {
+          passport.authenticate("local")(req, res, () => {
+            res.redirect("/secrets");
+          });
+        }
+      }
+    );
+  });
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    });
+  });
+
+app.route("/logout").get((req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// <---------- Used for level 5 ---------->
+
+/*
+// <---------- Used for level 1 through 4 ---------->
 app
   .route("/login")
   .get((req, res) => {
@@ -95,6 +186,7 @@ app
       }
     });
   });
+*/
 
 app.listen(5000, () => {
   console.log("Server started on Port 5000");
